449407fb45ae3a9f90f40ea31d34d29c62d5f26f
fabio.oliveira <fabiogoliveira.the@gmail.com>
2025-09-18 11:43:58 -0300

    primeiro commit do laboratorio vm

---
+++ files/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            Seu titulo aqui
+        </title>
+    </head>
+    <body>
+        Funcionou!!!! Copiou!!!
+        Copiou de novo mesmo o arquivo php!!
+        <?php
+        ini_set('disable_functions', '');
+        phpinfo();
+        ?>
+    </body>
+</html>
